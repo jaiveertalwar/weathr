7a99f5da7b8f2cce9acfc21bffaadb6686c0facc
Jaiveer Talwar <jaiveer_talwar@giftsthatgive.in>
2021-09-20 14:43:26 +0530

    upload

---
+++ precache-manifest.a460d8c73800fdf580893e268a5605d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "20c12e060dcaadada527",
+    "url": "/react-get-weather-app/static/css/main.bea25f8a.chunk.css"
+  },
+  {
+    "revision": "20c12e060dcaadada527",
+    "url": "/react-get-weather-app/static/js/main.20c12e06.chunk.js"
+  },
+  {
+    "revision": "7b6c2ae0059c41c95d85",
+    "url": "/react-get-weather-app/static/js/1.7b6c2ae0.chunk.js"
+  },
+  {
+    "revision": "fb1a151ed759a03dba9c",
+    "url": "/react-get-weather-app/static/js/runtime~main.fb1a151e.js"
+  },
+  {
+    "revision": "94cdfb126a2e86e5958c90f0739f635a",
+    "url": "/react-get-weather-app/static/media/app_logo.94cdfb12.png"
+  },
+  {
+    "revision": "9b11785de96fdeddd69f6152a6d8fe1e",
+    "url": "/react-get-weather-app/index.html"
+  }
+];
